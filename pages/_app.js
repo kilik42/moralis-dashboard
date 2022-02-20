4b5a7fd858bf9aa9e69b2df88b25ea7c6b276f9d
kilik42 <marvinevins@yahoo.com>
2022-02-19 23:05:39 -0600

    authentication

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
-
+import {MoralisProvider} from 'react-moralis';
+import {ChakraProvider} from '@chakra-ui/react';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      
+        <MoralisProvider
+          appId= {process.env.NEXT_APP_ID}
+          serverUrl= {process.env.NEXT_SERVER_URL}
+        >
+          <Component {...pageProps} />
+        </MoralisProvider>
+         
+
+
+
+      </ChakraProvider>
+    
+   
+
+    
+
+  )
 }
 
 export default MyApp
